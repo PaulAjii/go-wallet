9c6725f7334cccffed3fb702742169327fa047dc
PaulAjii <paulajii134@gmail.com>
2026-05-13 16:48:10 +0100

    feat: add base model structures for database interaction

---
+++ internal/models/base_model.go
@@ -0,0 +1,18 @@
+package models
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type BaseModel struct {
+	ID        uuid.UUID `json:"id" db:"id"`
+	CreatedAt time.Time `json:"createdAt" db:"created_at"`
+	UpdatedAt time.Time `json:"updatedAt" db:"updated_at"`
+}
+
+type BaseWithoutUpdatedAt struct {
+	ID        uuid.UUID `json:"id" db:"id"`
+	CreatedAt time.Time `json:"createdAt" db:"created_at"`
+}
