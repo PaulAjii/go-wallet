fc334ecdd95bf3d8bfb8f0fe4b0935167ad9c899
PaulAjii <paulajii134@gmail.com>
2026-05-13 16:47:49 +0100

    feat: implement database connection and pooling functionality

---
+++ pkg/database/db.go
@@ -0,0 +1,38 @@
+package database
+
+import (
+	"context"
+	"log"
+
+	"github.com/PaulAjii/go-wallet/pkg/config"
+	"github.com/PaulAjii/go-wallet/pkg/sysmsg"
+	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+var Pool *pgxpool.Pool
+
+func Connect() {
+	var err error
+
+	config, _ := pgxpool.ParseConfig(config.ApplicationConfig.Database.URL)
+
+	config.ConnConfig.StatementCacheCapacity = 0
+	config.ConnConfig.DefaultQueryExecMode = pgx.QueryExecModeSimpleProtocol
+
+	Pool, err = pgxpool.NewWithConfig(context.Background(), config)
+	if err != nil {
+		log.Fatalf("%s: %v\n", sysmsg.CannotConnect, err)
+	}
+
+	if err = Pool.Ping(context.Background()); err != nil {
+		log.Fatalf("%s: %v\n", sysmsg.CannotPing, err)
+	}
+
+	log.Println(sysmsg.ConnectionSuccessful)
+}
+
+func Close() {
+	Pool.Close()
+	log.Println(sysmsg.ConnectionClosed)
+}
