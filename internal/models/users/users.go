bdb108b92bf41d5f3d240acf22d898b88a2e8122
PaulAjii <paulajii134@gmail.com>
2026-05-13 17:22:29 +0100

    feat: add UsersModel with user details and verification status

---
+++ internal/models/users/users.go
@@ -0,0 +1,13 @@
+package users
+
+import "github.com/PaulAjii/go-wallet/internal/models"
+
+type UsersModel struct {
+	models.BaseModel
+
+	FullName   string `json:"fullName" db:"full_name"`
+	Username   string `json:"username" db:"username"`
+	Email      string `json:"email" db:"email"`
+	Password   string `json:"_" db:"password_hash"`
+	IsVerified bool   `json:"isVerified" db:"is_verified"`
+}
