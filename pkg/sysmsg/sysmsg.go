abcdc10b8fe21c6df9062a6b9ce639458eced94f
PaulAjii <paulajii134@gmail.com>
2026-05-13 16:48:22 +0100

    feat: add system message constants for environment variables and database connection statuses

---
+++ pkg/sysmsg/sysmsg.go
@@ -0,0 +1,14 @@
+package sysmsg
+
+// Environment Variables
+const (
+	NoEnvFile = "No .env file found"
+)
+
+// Database
+const (
+	CannotConnect        = "Unable to connect to database"
+	CannotPing           = "Unable to ping database"
+	ConnectionSuccessful = "Database connection successful"
+	ConnectionClosed     = "Database connection closed"
+)
