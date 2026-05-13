27f93c427d53b995764e398863c0f87ca5810308
PaulAjii <paulajii134@gmail.com>
2026-05-13 17:21:59 +0100

    feat: add authentication DTOs for register, login, and response handling

---
+++ internal/dtos/auth_dtos.go
@@ -0,0 +1,18 @@
+package dtos
+
+type RegisterRequest struct {
+	FullName string `json:"fullName"`
+	Username string `json:"username"`
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
+type LoginRequest struct {
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
+type AuthResponse struct {
+	Token string      `json:"token"`
+	User  interface{} `json:"user"`
+}
