625db43aabe1f23b41b64bf93e78071085523643
PaulAjii <paulajii134@gmail.com>
2026-05-15 00:52:56 +0100

    feat: add error and success messages for user authentication and account management

--- pkg/sysmsg/sysmsg.go
+++ pkg/sysmsg/sysmsg.go
@@ -12,3 +12,17 @@ const (
 	ConnectionSuccessful = "Database connection successful"
 	ConnectionClosed     = "Database connection closed"
 )
+
+const (
+	ErrBadReq                = "Invalid Request Body"
+	ErrInvalidCredentials    = "Email Or Password Is Invalid"
+	ErrInternalServerError   = "An Internal Server Error Occurred"
+	ErrEmailAlreadyExists    = "Email Already Exists"
+	ErrUsernameAlreadyExists = "Username Already Exists"
+	ErrInvalidPasswordLength = "Password Must Be At Least 8 Characters"
+)
+
+const (
+	CreationSuccess = "User created successfully"
+	LoginSuccess    = "User Logged In Successfully"
+)
