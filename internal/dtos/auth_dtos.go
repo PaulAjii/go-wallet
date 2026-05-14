848e132cb4ac5a51e1fcdedff439a3137c36d815
PaulAjii <paulajii134@gmail.com>
2026-05-14 17:36:26 +0100

    feat: update UserResponse structure and replace interface{} with UserResponse in AuthResponse

--- internal/dtos/auth_dtos.go
+++ internal/dtos/auth_dtos.go
@@ -7,12 +7,20 @@ type RegisterRequest struct {
 	Password string `json:"password"`
 }
 
+type UserResponse struct {
+	ID         string `json:"id"`
+	FullName   string `json:"fullName"`
+	Username   string `json:"username"`
+	Email      string `json:"email"`
+	IsVerified bool   `json:"isVerified"`
+}
+
 type LoginRequest struct {
 	Email    string `json:"email"`
 	Password string `json:"password"`
 }
 
 type AuthResponse struct {
-	Token string      `json:"token"`
-	User  interface{} `json:"user"`
+	Token string       `json:"token"`
+	User  UserResponse `json:"user"`
 }
