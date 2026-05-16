61eb072346aa027ecbf1e1b8936b3815b4eb8676
PaulAjii <paulajii134@gmail.com>
2026-05-16 18:47:58 +0100

    feat: add ProfileDTO struct for user profile data handling

---
+++ internal/dtos/user_dtos.go
@@ -0,0 +1,7 @@
+package dtos
+
+type ProfileDTO struct {
+	User        UserResponse          `json:"user_info"`
+	Wallet      WalletResponse        `json:"wallet"`
+	Transaction []TransactionResponse `json:"transaction"`
+}
