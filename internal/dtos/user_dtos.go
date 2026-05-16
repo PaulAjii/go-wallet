a83bef15da07fd77eb7738dd4e91624aba8874ae
PaulAjii <paulajii134@gmail.com>
2026-05-16 19:02:01 +0100

    fix: correct JSON key for transactions in ProfileDTO

--- internal/dtos/user_dtos.go
+++ internal/dtos/user_dtos.go
@@ -3,5 +3,5 @@ package dtos
 type ProfileDTO struct {
 	User        UserResponse          `json:"user_info"`
 	Wallet      WalletResponse        `json:"wallet"`
-	Transaction []TransactionResponse `json:"transaction"`
+	Transaction []TransactionResponse `json:"transactions"`
 }
